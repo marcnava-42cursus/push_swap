3a3187ef7980355c6ef0c9747d68bf6ed9e5bd0a
GreenLass000 <marcosnavarromartin@gmail.com>
2025-02-28 19:11:46 +0100

    checher started

--- checker/includes/checker.h
+++ checker/includes/checker.h
@@ -1,21 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   checker.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marcnava <marcnava@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/26 16:21:12 by marcnava          #+#    #+#             */
-/*   Updated: 2025/01/24 12:07:23 by marcnava         ###   ########.fr       */
+/*   Created: 2025/02/28 19:06:31 by marcnava          #+#    #+#             */
+/*   Updated: 2025/02/28 19:06:31 by marcnava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
-
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
-{
-	if (!lst || !del)
-		return ;
-	del(lst->content);
-	ft_free((void **)&lst);
-}
--- checker/src/checker.c
+++ checker/src/checker.c
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isblank.c                                       :+:      :+:    :+:   */
+/*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marcnava <marcnava@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 17:36:37 by marcnava          #+#    #+#             */
-/*   Updated: 2025/01/24 12:07:23 by marcnava         ###   ########.fr       */
+/*   Created: 2025/02/28 19:06:34 by marcnava          #+#    #+#             */
+/*   Updated: 2025/02/28 19:08:11 by marcnava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/libft.h"
+#include "../includes/checker.h"
 
-int	ft_isblank(int c)
+int main(int argc, char const *argv[])
 {
-	return (c == 32 || c == 9);
+	if (argc == 1)
+		argc = 1;
+	argv[0] = "hola";
+	return 0;
 }
